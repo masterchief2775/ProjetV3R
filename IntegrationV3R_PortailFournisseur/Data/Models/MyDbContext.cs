51797d31fd09eab86bb86bf752561fe0521e652b
Gabriel B. <sbrubicon12@hotmail.com>
2024-10-07 11:27:25 -0400

    Amélioration générale du formulaire de demande

--- IntegrationV3R_PortailFournisseur/Data/Models/MyDbContext.cs
+++ IntegrationV3R_PortailFournisseur/Data/Models/MyDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
 
-namespace IntegrationV3R_PortailFournisseur.Data.Models
-{
-    public class MyDbContext
-    {        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+//namespace IntegrationV3R_PortailFournisseur.Data.Models
+//{
+//    public class MyDbContext
+//    {        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
 
-        // Définis tes DbSet pour les entités ici
-        public DbSet<MonEntite> MonEntites { get; set; }
-    }
-}
+//        // Définis tes DbSet pour les entités ici
+//        public DbSet<MonEntite> MonEntites { get; set; }
+//    }
+//}
