b3f1de4d5185c7ccdd661317c4900c345d2274d4
FolLowCND <l.folcher426@gmail.com>
2024-10-21 11:16:07 -0400

    jpp de ce truc de con avec les gestions de session Blazor..
    Utilisation de Blazored.SessionStorage pour stocker les données de session sur le poste du client pour qu'il soit la de maniere permanante. Il pourra donc accéder a certaines pages (etat demande) que s'il est connecté. Sinon il sera redirigé vers la page de connexion.

--- IntegrationV3R_PortailFournisseur/Data/Models/ApplicationDbContext.cs
+++ IntegrationV3R_PortailFournisseur/Data/Models/ApplicationDbContext.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace IntegrationV3R_PortailFournisseur.Data.Models;
 
 public partial class ApplicationDbContext : DbContext
 {
+
     public ApplicationDbContext()
     {
     }
--- IntegrationV3R_PortailFournisseur/Program.cs
+++ IntegrationV3R_PortailFournisseur/Program.cs
@@ -2,37 +2,40 @@ using IntegrationV3R_PortailFournisseur.Data.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Blazored.SessionStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Ajouter les services à l'application.
 builder.Services.AddRazorPages();
-
-
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredSessionStorage();
 
 
+// Configuration des sessions
+builder.Services.AddHttpContextAccessor(); // Nécessaire pour accéder à HttpContext
+builder.Services.AddSession(); // Ajouter la gestion des sessions
+
+// Configuration de la base de données avec MySQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 18))));
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure le pipeline HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
-
-
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+// Utiliser les sessions dans l'application
+app.UseSession(); // Activer les sessions avant de les utiliser dans les composants
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
