f7cd0c39e89cd216b8aa316ef92da79b37379a65
TheTrashFellow <gporlier97@gmail.com>
2024-10-24 19:42:38 -0400

    Modal box reactiv && SingletonForm pour tout les inputs
    A revoir en equipe pour les dates de reception et dexpiration des licences rbq. Pas demande dans le devi

--- IntegrationV3R_PortailFournisseur/Shared/ComposantsFormulaire/SingletonFormulaire.cs
+++ IntegrationV3R_PortailFournisseur/Shared/ComposantsFormulaire/SingletonFormulaire.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntegrationV3R_PortailFournisseur.Data.Models;
+using System;
 
 namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
 {
@@ -25,6 +26,26 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
         public string NumeroTelephoneInput { get; set; } = string.Empty;
         public string SiteWebInput { get; set; } = string.Empty;
 
+        //Properties to hold data from contacts
+        public List<Contact> ContactsInput = new List<Contact>();
+
+        
+        // Properties to holld data from produits/services 
+        public string DescriptionProduitsServicesInput { get; set; } = string.Empty;
+
+        public List<UnspscComodite> ProduitsServicesSelectionnesInput = new List<UnspscComodite>();
+
+        //Properties to hold data from RBQ
+        public string RBQNumberInput {  get; set; } = string.Empty;
+        public string SelectedStatus { get; set; } = string.Empty;
+        public string SelectedLicenseType { get; set; } = string.Empty;
+        public string SelectedCategory { get; set; } = string.Empty;
+        public DateTime StartDateInput { get; set; } = DateTime.Now;
+        public DateTime EndDateInput { get; set; } = DateTime.Now;
+
+        public List<Souscategorieafter2008> SelectedSubCategories = new List<Souscategorieafter2008>();
+
+
         private SingletonFormulaire() { }
 
         public static SingletonFormulaire Instance
@@ -47,7 +68,9 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
 
         public void LogDataToConsole()
         {
+            Console.WriteLine("*****************************************************************************************");
             // Log identification data
+            Console.WriteLine("\n***IDENTIFICATION***");
             Console.WriteLine($"Nom de l'entreprise: {NomEntrepriseInput}");
             Console.WriteLine($"NEQ: {NeqInput}");
             Console.WriteLine($"Email: {EmailInput}");
@@ -56,6 +79,7 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
             Console.WriteLine($"Répéter le mot de passe: {RepeatPasswordInput}");
 
             // Log address data
+            Console.WriteLine("\n***ADRESSE***");
             Console.WriteLine($"Numéro Civique: {NumCiviqueInput}");
             Console.WriteLine($"Bureau: {BureauInput}");
             Console.WriteLine($"Rue: {RueInput}");
@@ -65,6 +89,55 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
             Console.WriteLine($"Région: {RegionInput}");
             Console.WriteLine($"Numéro de téléphone: {NumeroTelephoneInput}");
             Console.WriteLine($"Site Web: {SiteWebInput}");
+
+            // Log Contacts
+            Console.WriteLine("\n***LISTE DES CONTACTS***");
+            foreach (Contact contact in ContactsInput)
+            {
+                Console.WriteLine($"\n\tNom complet: {contact.Prenom} {contact.Nom} \n\tFonction: {contact.Role} \n\tEmail: {contact.Email} \n\t" +
+                    $"Telephone: {contact.NumeroTelephone} Poste {contact.Poste} - {contact.TypeTelephone}");
+            }
+            // Log Produits
+            Console.WriteLine("\n***PRODUITS***");
+            Console.WriteLine($"Description des produits et services offerts:\n\t {DescriptionProduitsServicesInput}");
+            Console.WriteLine("Liste des produits et services selectionnees\n");
+            foreach (UnspscComodite produit in ProduitsServicesSelectionnesInput) 
+            {
+                Console.WriteLine($"\t{produit.ComoditeNombre} - {produit.ComoditeTitreFr}");
+            }
+
+            // Log RBQ
+            Console.WriteLine("\n***RBQ***");
+            Console.WriteLine($"Code RBQ : {RBQNumberInput}");
+            Console.WriteLine($"Statut Licence : {SelectedStatus}");
+            Console.WriteLine($"Type Licence : {SelectedLicenseType}");
+            Console.WriteLine($"Type Licence : {SelectedCategory}");
+            foreach(var sousCategorie in SelectedSubCategories)
+            {
+                Console.WriteLine($"\t{sousCategorie.NumeroSousCategorieAfter2008} - {sousCategorie.NomSousCategorieAfter2008}");
+            }
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
+    }
+
+    public class Contact
+    {
+        public string Prenom { get; set; } = string.Empty;
+        public string Nom { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string NumeroTelephone { get; set; } = string.Empty;
+        public string TypeTelephone { get; set; } = string.Empty;
+        public string Poste { get; set; } = string.Empty;
+
+        // Error messages
+        public string PrenomError { get; set; } = string.Empty;
+        public string NomError { get; set; } = string.Empty;
+        public string RoleError { get; set; } = string.Empty;
+        public string EmailError { get; set; } = string.Empty;
+        public string NumeroTelephoneError { get; set; } = string.Empty;
+        public string TypeTelephoneError { get; set; } = string.Empty;
+        public string PosteError { get; set; } = string.Empty;
     }
 }
