1296e0f79aa9f886672057b7e19e84a70173d1bd
Gabriel Porlier <gabriel.porlier.01@edu.cegeptr.qc.ca>
2024-11-06 17:53:09 -0500

    Fix la situation SingletonForm
    Rendu à confirmer changements BD

--- IntegrationV3R_PortailFournisseur/Shared/ComposantsFormulaire/SingletonFormulaire.cs
+++ IntegrationV3R_PortailFournisseur/Shared/ComposantsFormulaire/SingletonFormulaire.cs
@@ -1,11 +1,12 @@
 ﻿using IntegrationV3R_PortailFournisseur.Data.Models;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
 {
     public class SingletonFormulaire
     {
-        private static SingletonFormulaire _instance;
         private static readonly object _lock = new object();
 
         // Properties to hold form data for identification
@@ -26,17 +27,15 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
         public string NumeroTelephoneInput { get; set; } = string.Empty;
         public string SiteWebInput { get; set; } = string.Empty;
 
-        //Properties to hold data from contacts
-        public List<Contact> ContactsInput = new List<Contact>();
+        // Properties to hold data from contacts
+        public List<ContactInput> ContactsInput = new List<ContactInput>();
 
-        
-        // Properties to holld data from produits/services 
+        // Properties to hold data from produits/services 
         public string DescriptionProduitsServicesInput { get; set; } = string.Empty;
-
         public List<UnspscComodite> ProduitsServicesSelectionnesInput = new List<UnspscComodite>();
 
-        //Properties to hold data from RBQ
-        public string RBQNumberInput {  get; set; } = string.Empty;
+        // Properties to hold data from RBQ
+        public string RBQNumberInput { get; set; } = string.Empty;
         public string SelectedStatus { get; set; } = string.Empty;
         public string SelectedLicenseType { get; set; } = string.Empty;
         public string SelectedCategory { get; set; } = string.Empty;
@@ -45,26 +44,7 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
 
         public List<Souscategorieafter2008> SelectedSubCategories = new List<Souscategorieafter2008>();
 
-
-        private SingletonFormulaire() { }
-
-        public static SingletonFormulaire Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    lock (_lock)
-                    {
-                        if (_instance == null)
-                        {
-                            _instance = new SingletonFormulaire();
-                        }
-                    }
-                }
-                return _instance;
-            }
-        }
+        public SingletonFormulaire() { }
 
         public void LogDataToConsole()
         {
@@ -74,7 +54,6 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
             Console.WriteLine($"Nom de l'entreprise: {NomEntrepriseInput}");
             Console.WriteLine($"NEQ: {NeqInput}");
             Console.WriteLine($"Email: {EmailInput}");
-            // Be cautious with logging sensitive data like passwords
             Console.WriteLine($"Mot de passe: {PasswordInput}");
             Console.WriteLine($"Répéter le mot de passe: {RepeatPasswordInput}");
 
@@ -92,16 +71,17 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
 
             // Log Contacts
             Console.WriteLine("\n***LISTE DES CONTACTS***");
-            foreach (Contact contact in ContactsInput)
+            foreach (ContactInput contact in ContactsInput)
             {
                 Console.WriteLine($"\n\tNom complet: {contact.Prenom} {contact.Nom} \n\tFonction: {contact.Role} \n\tEmail: {contact.Email} \n\t" +
-                    $"Telephone: {contact.NumeroTelephone} Poste {contact.Poste} - {contact.TypeTelephone}");
+                                  $"Telephone: {contact.NumeroTelephone} Poste {contact.Poste} - {contact.TypeTelephone}");
             }
+
             // Log Produits
             Console.WriteLine("\n***PRODUITS***");
             Console.WriteLine($"Description des produits et services offerts:\n\t {DescriptionProduitsServicesInput}");
             Console.WriteLine("Liste des produits et services selectionnees\n");
-            foreach (UnspscComodite produit in ProduitsServicesSelectionnesInput) 
+            foreach (UnspscComodite produit in ProduitsServicesSelectionnesInput)
             {
                 Console.WriteLine($"\t{produit.ComoditeNombre} - {produit.ComoditeTitreFr}");
             }
@@ -111,17 +91,86 @@ namespace IntegrationV3R_PortailFournisseur.Shared.ComposantsFormulaire
             Console.WriteLine($"Code RBQ : {RBQNumberInput}");
             Console.WriteLine($"Statut Licence : {SelectedStatus}");
             Console.WriteLine($"Type Licence : {SelectedLicenseType}");
-            Console.WriteLine($"Type Licence : {SelectedCategory}");
-            foreach(var sousCategorie in SelectedSubCategories)
+            Console.WriteLine($"Categorie : {SelectedCategory}");
+            foreach (var sousCategorie in SelectedSubCategories)
             {
                 Console.WriteLine($"\t{sousCategorie.NumeroSousCategorieAfter2008} - {sousCategorie.NomSousCategorieAfter2008}");
             }
 
             Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
+
+        public async Task SaveDataAsync(ApplicationDbContext dbContext)
+        {
+            // Création de l'entité Fournisseur
+            var fournisseur = new Fournisseur
+            {
+                NomEntreprise = this.NomEntrepriseInput,
+                Neq = this.NeqInput,
+                CourrielEntreprise = this.EmailInput,
+                EtatDemande = "En attente",
+                DateInscription = DateTime.Now
+            };
+            dbContext.Fournisseurs.Add(fournisseur);
+            await dbContext.SaveChangesAsync();
+
+            // Création de l'adresse
+            var adresse = new Adress
+            {
+                NumeroCivique = this.NumCiviqueInput,
+                Bureau = this.BureauInput,
+                Rue = this.RueInput,
+                Ville = this.VilleInput,
+                Province = this.ProvinceInput,
+                CodePostal = this.CodePostalInput,
+                FournisseurId = fournisseur.FournisseurId
+            };
+            dbContext.Adresses.Add(adresse);
+
+
+            // Ajouter les contacts
+            foreach (var contactInput in this.ContactsInput)
+            {
+                var contact = new Contact
+                {
+                    PrenomContact = contactInput.Prenom,
+                    NomContact = contactInput.Nom,
+                    FonctionContact = contactInput.Role,
+                    CourrielContact = contactInput.Email,
+                    NumTelContact = contactInput.NumeroTelephone,
+                    TypeTel = contactInput.TypeTelephone,
+                    PosteTelContact = contactInput.Poste,
+                    FournisseurId = fournisseur.FournisseurId
+                };
+                dbContext.Contacts.Add(contact);
+            }
+
+            // Ajouter les produits/services
+            foreach (var produit in this.ProduitsServicesSelectionnesInput)
+            {
+                var produitService = new Produitsservice
+                {
+                    Details = this.DescriptionProduitsServicesInput,
+                    FournisseurId = fournisseur.FournisseurId
+                };
+                dbContext.Produitsservices.Add(produitService);
+            }
+
+            // Ajouter les informations RBQ
+            var rbq = new Rbq
+            {
+                NumLicence = this.RBQNumberInput,
+                StatutLicence = this.SelectedStatus,
+                FournisseurId = fournisseur.FournisseurId
+            };
+            dbContext.Rbqs.Add(rbq);
+
+            // Sauvegarder les modifications
+            await dbContext.SaveChangesAsync();
+        }
     }
 
-    public class Contact
+    public class ContactInput
     {
         public string Prenom { get; set; } = string.Empty;
         public string Nom { get; set; } = string.Empty;
