ebe3e063d565c62430c8f236c0e8201bdc17b2dd
Gabriel B. <sbrubicon12@hotmail.com>
2024-12-01 21:07:13 -0500

    Bug fix

---
+++ IntegrationV3R_PortailFournisseur/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿window.onbackbutton = function (callback) {
+    window.onpopstate = function (event) {
+        if (confirm("Vous allez perdre votre progrès si vous retournez à la page de connexion \nÊtes-vous certain de vouloir retourner à la connexion ?")) {
+            callback();
+        } else {
+            history.pushState(null, null, location.href); // Prevent back navigation
+        }
+    };
+};
